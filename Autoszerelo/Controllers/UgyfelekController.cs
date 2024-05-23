7492daa2d729a404ed93b0ae3e6474a334299115
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-23 16:27:09 +0200

    ugyfel controller created

---
+++ Autoszerelo/Controllers/UgyfelekController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Autoszerelo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UgyfelekController : ControllerBase
+    {
+
+    }
+}
