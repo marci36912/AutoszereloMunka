61e7f8ab699e5603f6e248586551ce615b3d96d1
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-23 15:18:26 +0200

    a few properties added to munka

--- Autoszerelo/DataClasses/Munka.cs
+++ Autoszerelo/DataClasses/Munka.cs
@@ -2,5 +2,16 @@
 {
     public class Munka
     {
+        public Guid MunkaAzonosito { get; private set; }
+        public Guid UgyfelSzam {  get; private set; }
+        //regex
+        public string Rendszam { get; private set; }
+        //validacio
+        public DateOnly GyartasiEv {  get; private set; }
+        //munka kategoria
+        public string HibaRovidLeirasa { get; private set; }
+        //validacio
+        public int HibaSulyossaga { get; private set; }
+        //munka allapota
     }
 }
