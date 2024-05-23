a4c552ae372b135d6496b6533afe124fb7233701
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-23 15:13:34 +0200

    properties added to ugyfel

--- Autoszerelo/DataClasses/Ugyfel.cs
+++ Autoszerelo/DataClasses/Ugyfel.cs
@@ -2,5 +2,10 @@
 {
     public class Ugyfel
     {
+        public Guid Ugyfelszam {  get; private set; }
+        public string Nev {  get; private set; }
+        public string Lakcim { get; private set; }
+        //validacio
+        public string Email { get; private set; }
     }
 }
