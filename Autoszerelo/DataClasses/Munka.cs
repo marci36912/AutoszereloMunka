5354c49d93abdf8facd347268b6581e54368051c
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-23 18:26:12 +0200

    validations added to munka, and date validator class created

--- Autoszerelo/DataClasses/Munka.cs
+++ Autoszerelo/DataClasses/Munka.cs
@@ -1,16 +1,19 @@
-﻿namespace Autoszerelo.DataClasses
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autoszerelo.DataClasses
 {
     public class Munka
     {
         public Guid MunkaAzonosito { get; private set; }
         public Guid UgyfelSzam {  get; private set; }
-        //regex
+        [RegularExpression("^[A-Z]{3}-[0-9]{3}$")]
         public string Rendszam { get; private set; }
         //validacio
         public DateOnly GyartasiEv {  get; private set; }
         public MunkaKategoria MunkaKategoria { get; private set; }
         public string HibaRovidLeirasa { get; private set; }
         //validacio
+        [Range(1,10)]
         public int HibaSulyossaga { get; private set; }
         public MunkaAllapot MunkaAllapot { get; private set; }
     }
---
+++ Autoszerelo/Validator/DateDataAnnotationValidator.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autoszerelo.Validator
+{
+    public class DateDataAnnotationValidator : ValidationAttribute
+    {
+    }
+}
