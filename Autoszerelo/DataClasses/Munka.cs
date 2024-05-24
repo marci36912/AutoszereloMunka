f57af5f5f3cb2420e26ea52e5772d4bdd29058f9
Laczik_Marton <lmarton1@mailbox.unideb.hu>
2024-05-24 15:27:01 +0200

    typo fixed

--- Autoszerelo/DataClasses/Munka.cs
+++ Autoszerelo/DataClasses/Munka.cs
@@ -10,7 +10,7 @@ namespace Autoszerelo.DataClasses
         public Guid MunkaAzonosito { get; set; }
         [ForeignKey("Ugyfelek")]
         public Guid UgyfelSzam {  get; set; }
-        [RegularExpression("^[A-Z]{3}-((00[1-9])|(0[1-9][0-9])|([1-9][0-9]{2}))$", ErrorMessage = "Nem megfelelo formatum! Pelda helyes hasznalatra: AAA-001")]
+        [RegularExpression("^[A-Z]{3}-((00[1-9])|(0[1-9][0-9])|([1-9][0-9]{2}))$", ErrorMessage = "Nem megfelelo formatum! Pelda helyes hasznalata: AAA-001")]
         public string Rendszam { get; set; } = null!;
         [Range(typeof(DateOnly), "1900-01-01", "2030-01-01")]
         public DateOnly GyartasiEv {  get; set; }
